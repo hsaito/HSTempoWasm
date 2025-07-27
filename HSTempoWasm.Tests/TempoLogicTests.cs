ffc64ed8069c56cd99510d7a3c4500da0b2f1bd0
Hideki Saito <hidekis@gmail.com>
2025-07-27 04:38:59 -0700

    File structure changes

---
+++ HSTempoWasm.Tests/TempoLogicTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using FluentAssertions;
+using HSTempoWasm;
+
+namespace HSTempoWasm.Tests
+{
+    public class TempoLogicTests
+    {
+        [Theory]
+        [InlineData(120, 500)]
+        [InlineData(60, 1000)]
+        [InlineData(0, 0)]
+        public void CalculateBPMtoMS_ReturnsExpectedMilliseconds(double bpm, short expectedMs)
+        {
+            // Act
+            var result = TempoLogic.CalculateBPMtoMS(bpm);
+
+            // Assert
+            result.Should().Be(expectedMs);
+        }
+    }
+}
